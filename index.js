d1f90757cc8d897d74fc1bfff85b45bd853248c4
Juan Gatto <jgatto@correo.um.edu.uy>
2018-05-22 13:07:14 -0300

    routes

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
-const express = require('express')
-const path = require('path')
+var express = require('express');
+
+var index = require('./routes/index');
+var locales = require('./routes/locales');
+var app = express();
 const PORT = process.env.PORT || 5000
 
-const mongoose = require("mongoose");
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
-var uristring =
-  process.env.MONGODB_URI ||
-  'mongodb://localhost/HelloMongoose';
+app.use('/', index);
+app.use('/locales', locales);
 
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(uristring, function(err, res) {
-  if (err) {
-    console.log('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log('Succeeded connected to: ' + uristring);
-  }
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var comercioSchema = new mongoose.Schema({
-  nombre: String,
-  direccion: String,
-  coord_x: Number,
-  coord_y: Number
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var Comercio = mongoose.model('Comercios', comercioSchema);
-
-// Creating one user.
-var abitab = new Comercio({
-  nombre: 'Abitab 25-00',
-  direccion: 'Av. Gral. Rivera 2406, 11300 Montevideo',
-  coord_x: -34.902690,
-  coord_y: -56.163212
-});
-
-// Saving it to the database.
-abitab.save(function(err) {
-  if (err) console.log('Error on save!')
-});
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/ok', (req, res) => handleOk(res))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-function handleOk(res) {
-  var query = Comercio.find({
-    'nombre': 'Abitab 25-00'
-  });
-  query.exec(function(err, result) {
-    if (!err) {
-      res.end(JSON.stringify(result, undefined, 2));
-    } else {
-      res.end('Error in second query. ' + err)
-    }
-  });
-}
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
